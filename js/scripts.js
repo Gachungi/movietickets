464748c482c03df8d045cf1f6e92ca8b48a5d4f0
Gathua Kiragu <kiragugathua@gmail.com>
2017-02-28 17:15:43 +0300

    Added Some javascript function

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function Ticket(movieTitle, movieType, movieTime, userAge) {
+    this.movieTitle = movieTitle;
+    this.movieType = movieType;
+    this.movieTime = movieTime;
+    this.userAge = userAge;
+}
+
