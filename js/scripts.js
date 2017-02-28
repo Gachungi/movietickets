343a4b5abd81ccd141f35d482a78c2aac4729c5a
Gathua Kiragu <kiragugathua@gmail.com>
2017-02-28 17:16:46 +0300

    Jquery and Declared Some variables

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,19 @@ function Ticket(movieTitle, movieType, movieTime, userAge) {
     this.userAge = userAge;
 }
 
+Ticket.prototype.ticketPrice = function () {
+    return this.movieTitle + this.movieTime + this.movieType + this.userAge;
+}
+
+$(document).ready(function() {
+    event.preventDefault();
+    $("form#new-ticket").submit(function(event) {
+        event.preventDefault();
+
+        var inputTitle = parseInt($("select.new-movieTitle").val());
+        var inputTime = parseInt($("select.new-movieTime").val());
+        var inputAge = parseInt($("select.new-userAge").val());
+        var inputType= parseInt($("select.new-movieType").val());
+
+        var newTicket = new Ticket(inputTitle, inputTime, inputAge, inputType);
+
